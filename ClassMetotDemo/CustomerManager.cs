040444abb93d1f37f3222fe8aa2149d025c388d5
Faruk Onar <farukoonar@gmail.com>
2021-01-27 16:15:34 +0300

    Add project files.

---
+++ ClassMetotDemo/CustomerManager.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClassMetotDemo
+{
+    class CustomerManager
+    {
+        public void Add(Customer customer)
+        {
+            Console.WriteLine(customer.Name +" added!");
+        }
+        public void Delete(Customer customer)
+        {
+            Console.WriteLine(customer.Name + " deleted!");
+        }
+        public void Update(Customer customer)
+        {
+            Console.WriteLine(customer.Name +" updated!");
+        }
+    }
+}
---
+++ ClassMetotDemo/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace ClassMetotDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Customer customer1 = new Customer { Id = 1, Name = "Engin", LastName = "Demiroğ", CustomerNumber = "265478" };
+            Customer customer2 = new Customer { Id = 2, Name = "Faruk", LastName = "Onar", CustomerNumber = "265487" };
+
+            CustomerManager customerManager = new CustomerManager();
+            customerManager.Add(customer1);
+            customerManager.Delete(customer2);
+            customerManager.Update(customer1);
+        }
+    }
+}
